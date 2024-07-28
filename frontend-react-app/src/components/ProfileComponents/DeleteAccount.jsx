ff4dd074879021371fd37126d670211cde05f6a4
Waseq225 <mohammadwaseq225@gmail.com>
2024-07-28 11:24:49 +1000

    delete user from firebase

--- frontend-react-app/src/components/ProfileComponents/DeleteAccount.jsx
+++ frontend-react-app/src/components/ProfileComponents/DeleteAccount.jsx
@@ -1,24 +1,35 @@
-import { Button } from "@mui/material"
-import axios from "axios"
-import { useContext } from "react"
-import { UserContext } from "../../userContext"
+import { Button } from '@mui/material'
+import axios from 'axios'
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { SnackbarContext } from '../../snackbarContext'
+import { UserContext } from '../../userContext'
+import { getAuth, deleteUser } from 'firebase/auth'
 
 export const DeleteAccount = () => {
-const {user} = useContext(UserContext)
+    const { user, setUser } = useContext(UserContext)
+    const { setToastOpen, setToastText } = useContext(SnackbarContext)
 
-const handleDeleteUser = async (ev) =>{
-  ev.preventDefault()
-  await axios.
-  delete(`/user/delete/${user._id}`).then(()=>{
-    alert("User deleted!")
-})
-  .catch((e)=>{
-    alert(e.message)
-  })
-  
-}
+    const navigate = useNavigate()
 
-  return (
-    <Button onClick={handleDeleteUser}>Cancel account</Button>
-  )
+    const auth = getAuth()
+    const firebaseUser = auth.currentUser
+
+    const handleDeleteUser = async (ev) => {
+        ev.preventDefault()
+
+        deleteUser(firebaseUser)
+            .then(() => axios.delete(`/user/delete/${user._id}`))
+            .then(() => {
+                setUser(null)
+                setToastText('User deleted')
+                setToastOpen(true)
+                navigate('/')
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    return <Button onClick={handleDeleteUser}>Cancel account</Button>
 }
